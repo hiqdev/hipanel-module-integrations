9470e6cc260434411b2845e1bc3e23e6065b3966
pkolomiy <pkolomiy@gmail.com>
2018-06-06 14:55:12 +0300

    Added files for codeception tests

---
+++ tests/_bootstrap.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Integrations management plugin for HiPanel
+ *
+ * @link      https://github.com/hiqdev/hipanel-module-integrations
+ * @package   hipanel-module-integrations
+ * @license   BSD-3-Clause
+ * @copyright Copyright (c) 2017, HiQDev (http://hiqdev.com/)
+ */
+
+error_reporting(E_ALL & ~E_NOTICE);
+
+require_once __DIR__ . '/../vendor/autoload.php';
