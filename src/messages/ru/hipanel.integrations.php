0bce32ac126530b28cabb9a179c82dc13f601774
strorch <smy980807@ukr.net>
2019-08-20 15:48:12 +0300

    added disabling and enabling integrations

--- src/messages/ru/hipanel.integrations.php
+++ src/messages/ru/hipanel.integrations.php
@@ -28,4 +28,8 @@ return [
     'Password 2' => 'Пароль 2',
     'Access' => 'Доступ',
     'Information about integration' => 'Подробная информация',
+    'Failed to disable integrations' => 'Ошибка при отключении интеграций',
+    'Failed to enable integrations' => 'Ошибка при включении интеграций',
+    'Integrations has been disabled' => 'Инетграции отключены',
+    'Integrations has been enabled' => 'Инетграции включены',
 ];
